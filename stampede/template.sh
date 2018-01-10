d8b050744ff58ded90a8d19ffdf07cb0d2a63a3d
Joshua Lynch <jklynch@email.arizona.edu>
2018-01-10 17:51:38 -0500

    double quote the CMD_LINE variable because it contains spaces

--- stampede/template.sh
+++ stampede/template.sh
@@ -6,7 +6,7 @@ echo "BC_NAME       \"${BC_NAME}\""
 echo "CMD_LINE      \"${CMD_LINE}\""
 
 time sh run.sh \
-    ${BC_NAME} ${CMD_LINE}
+    ${BC_NAME} "${CMD_LINE}"
 
 echo "Ended iMicrobe Puca template.sh $(date)"
 exit 0
